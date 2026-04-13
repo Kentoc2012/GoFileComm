db1d300c202410c5c6d41e21489c07e4ce746bcb
Kentoc <149084406+Kentoc2012@users.noreply.github.com>
2026-04-13 20:50:55 +0300

    Add files via upload

--- GoFileComm.go
+++ GoFileComm.go
@@ -5,21 +5,39 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
+)
+
+const ( //цветовые коды
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Cyan   = "\033[36m"
 )
 
 func main() {
 	for {
 		var an1 string
-		fmt.Println("удалить, прочитать, создать файл, создать папку или просмотреть деректорию? (1/2/3/4/5)") //меню
+		fmt.Println("удалить, прочитать, создать файл, создать папку, просмотреть деректорию, выполнить файл или показать дополнительный текст? (1/2/3/4/5/6/help) напишите exit для выхода") //меню
 		fmt.Scan(&an1)
+		if an1 == "help" || an1 == "h" {
+			fmt.Println("GoFileComm, это файловый менеджер созданный новичком в Go, так что не судите строго\n Функционал:\n 1. удаление файлов\n 2. прочтение файлов (не знаю зачем, но пусть будет)\n 3. создание файлов\n 4. создание папки\n 5. просмотр деректории (папки)\n 6. выполнение файла\n \n разделение создания файла и деректории обусловленно удобством, чтобы не дописывать всегда .dir в конце названия.")
+			continue
+		}
+		if an1 == "exit" || an1 == "e" {
+			return
+		}
 		an, err := strconv.Atoi(an1)
 		if err != nil {
-			fmt.Println("errot", err)
+			fmt.Println(Red+"errot", err)
 		}
-		if an != 1 && an != 2 && an != 3 && an != 4 && an != 5 {
-			fmt.Println("разрешены только варианты в меню!")
+
+		if an != 1 && an != 2 && an != 3 && an != 4 && an != 5 && an != 6 {
+			fmt.Println(Green + "разрешены только варианты в меню!" + Reset)
 			continue
 		}
 		if an == 1 { //удаление файлов
@@ -29,7 +47,10 @@ func main() {
 			finalpath := filepath.Join(name)
 			err := os.Remove(finalpath)
 			if err != nil {
-				fmt.Println("error", err)
+				fmt.Println(Red+"error"+Reset, err)
+
+			} else {
+				fmt.Println(Green + "удаление завершено" + Reset)
 			}
 		}
 		if an == 2 { //прочетние файлов (не знаю зачем, но пусть будет)
@@ -39,9 +60,9 @@ func main() {
 			finalpath := filepath.Join(name)
 			text, err := ioutil.ReadFile(finalpath)
 			if err != nil {
-				fmt.Println("error", err)
+				fmt.Println(Red+"error"+Reset, err)
 			}
-			fmt.Println("текст файла:\n", string(text))
+			fmt.Println("текст файла:\n", Yellow+string(text)+Reset)
 		}
 		if an == 4 { //создание папки (да 4, не 3)
 			var dirname string
@@ -56,12 +77,12 @@ func main() {
 			}
 			err := os.Mkdir(finalpath, 0755)
 			if err != nil {
-				fmt.Println("ошибка создания файлов", err)
+				fmt.Println(Red+"ошибка создания папки"+Reset, err)
 			}
 		}
 		if an == 5 { //просмотр папки (dir в консоли)
 			var path string
-			fmt.Println("введите путь для просмотра (или . для тукущей папки)")
+			fmt.Println(Cyan + "введите путь для просмотра (или . для тукущей папки)" + Reset)
 			fmt.Scan(&path)
 			var finalpath string
 			if !filepath.IsAbs(path) {
@@ -74,11 +95,11 @@ func main() {
 			if err != nil {
 				fmt.Println("\033[31m[ERROR]\033[0m ошибка сканирования", err)
 			} else {
-				fmt.Println("Содержимое директории:", finalpath)
+				fmt.Println(Cyan+"Содержимое директории:", finalpath)
 				fmt.Println("-----------------------------------")
 				for _, file := range files {
 					if file.IsDir() {
-						fmt.Println("\033[34m[DIR]\033[0m ", file.Name())
+						fmt.Println("\033[34m[DIR]\033[0m ", file.Name()+Reset)
 					} else {
 						fmt.Println("[FILE] ", file.Name())
 					}
@@ -86,11 +107,30 @@ func main() {
 				fmt.Println("-----------------------------------")
 			}
 		}
+		if an == 6 {
+			var finalpath string
+			var filename string
+			fmt.Println("укажите путь для запуска или название, если он находится в текущей папке")
+			fmt.Scan(&filename)
+			if !filepath.IsAbs(filename) {
+				currentdir, _ := os.Getwd()
+				finalpath = filepath.Join(currentdir, filename)
+			} else {
+				finalpath = filename
+			}
+			cmd := exec.Command("cmd", "/C", "start", "", finalpath)
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println("\033[31m[ERROR]\033[0m ошибка запуска:", err)
+			} else {
+				fmt.Println(Blue + "запуск произведён успешно")
+			}
+		}
 		if an == 3 { //создание файлов (да, вот и 3)
 			scanner := bufio.NewScanner(os.Stdin)
 			var text string
 			var name string
-			fmt.Println("введи название файла или путь")
+			fmt.Println("введите название файла или путь")
 			fmt.Scan(&name)
 			currentdir, _ := os.Getwd()
 			scanner.Scan() //продувка буфера от предыдущего Enter
@@ -106,9 +146,9 @@ func main() {
 			}
 			err := ioutil.WriteFile(finalpath, text1, 0644) //создание файла
 			if err != nil {
-				fmt.Println("error\n", err)
+				fmt.Println(Red+"error\n", err)
 			}
-			fmt.Println("путь к файлу:", finalpath)
+			fmt.Println(Yellow+"путь к файлу:"+Reset, finalpath)
 		}
 	}
 }
